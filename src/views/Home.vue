1d98ce2b019044088ea21505ac817bbef2371399
shenjianing <893775937@qq.com>
2021-01-12 16:02:01 +0800

    fist commit - 新增todoList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todolist">
+    <div style="margin-top: 15px;">
+      <el-input type="text" 
+        v-model="todo" 
+        placeholder="edit me"
+        @keyup.enter.native="addToDo">
+        <template slot="append">
+          <el-button type="success" icon="el-icon-check" circle @click="addToDo"></el-button>
+        </template>
+      </el-input>
+      
+      
+    </div>
+    
+    <ul class="list">
+      <li
+        v-for="(item, index) in todoList"
+        :key="index"
+        @click="deleteTo(index)"
+      >{{ item }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todo: '',
+      todoList: (localStorage.getItem("todolis") != null) ? eval(localStorage.getItem("todolis")) : [],
+    }
+  },
+  methods: {
+    addToDo() {
+      this.todoList.push(this.todo);
+      this.todo = '';
+      localStorage.setItem("todolis", JSON.stringify(this.todoList));
+      // console.log(this.todoList)
+      
+    },
+    deleteTo(index) {
+      // console.log('delete', index)
+      this.todoList.splice(index,1);
+      localStorage.setItem("todolis", JSON.stringify(this.todoList));
+    }
+
   }
+ 
 }
 </script>
+<style scoped>
+.list {
+  list-style: none; 
+}
+</style>
